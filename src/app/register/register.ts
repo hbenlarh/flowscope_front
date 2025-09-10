264a54cb9273c40bbc5bd53b87396536f94b7eb5
hajar <hajarbenlarhzal@gmail.com>
2025-09-10 17:26:18 +0100

    adding signup

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -4,23 +4,26 @@ import { Footer } from '../shared/footer/footer';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [Header, Footer, FormsModule, ReactiveFormsModule,CommonModule],
+  imports: [Header, Footer, FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './register.html',
   styleUrl: './register.scss'
 })
 export class Register implements OnInit {
   signupForm!: FormGroup;
-  signupData = { username: '', password: '', email: '', role: '' };
+  signupData = { first_name: '', last_name: '', password: '', email: '', role: '' };
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient, private router: Router) { }
 
   ngOnInit(): void {
     // Initialize the form group with validation
     this.signupForm = this.fb.group({
-      username: ['', [Validators.required, Validators.minLength(3)]],
+      first_name: ['', [Validators.required, Validators.minLength(3)]],
+      last_name: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]],
       confirmPassword: ['', [Validators.required]]
@@ -34,7 +37,28 @@ export class Register implements OnInit {
     const confirmPassword = formGroup.get('confirmPassword')?.value;
     return password === confirmPassword ? null : { mismatch: true };
   }
-    signup() {
+  signup() {
+    this.signupData = {
+      first_name: this.signupForm.get('first_name')?.value,
+      last_name: this.signupForm.get('last_name')?.value,
+      password: this.signupForm.get('password')?.value,
+      email: this.signupForm.get('email')?.value,
+      role: 'user'
+    };
+    console.log('Request Data:', this.signupData);
+    this.http.post('/api/client', this.signupData, { withCredentials: true })
+      .subscribe(
+        (response) => {
+          console.log("hello");
+          console.log(response);
+          console.log(this.signupData);
+          this.router.navigate(['']); // Navigate after successful signup
+        },
+        (error) => {
+          console.error(error);
+          console.log("im here bad");
+        }
+      );
+  }
 
-    }
 }
